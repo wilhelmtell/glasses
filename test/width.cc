8243a7829b81e6dfb21be294676218c3e678ef6f
Matan Nassau <matan.nassau@gmail.com>
2015-04-08 19:28:10 -0400

    const-correctness in tests

--- test/width.cc
+++ test/width.cc
@@ -2,17 +2,17 @@
 #include "../src/width.hh"
 
 TEST_CASE("value-initialized width equals 0") {
-  tls::width w{};
+  tls::width const w{};
   REQUIRE(w == 0);
 }
 
 TEST_CASE("direct-initialized width equals the value it was initialized to") {
-  tls::width w{4};
+  tls::width const w{4};
   REQUIRE(w == 4);
 }
 
 TEST_CASE("width::value_type compiles") {
-  tls::width::value_type x{4};
-  tls::width w{4};
+  tls::width::value_type const x{4};
+  tls::width const w{4};
   REQUIRE(x == w);
 }
