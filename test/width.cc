2a1d68b269c569aa0edb9bdfd2fad8b86cf00a7c
Matan Nassau <matan.nassau@gmail.com>
2015-04-08 19:27:31 -0400

    test width direct-initialization

--- test/width.cc
+++ test/width.cc
@@ -5,3 +5,8 @@ TEST_CASE("value-initialized width equals 0") {
   tls::width w{};
   REQUIRE(w == 0);
 }
+
+TEST_CASE("direct-initialized width equals the value it was initialized to") {
+  tls::width w{4};
+  REQUIRE(w == 4);
+}
