fb0b2764e759996f853179f6f460968a054c78eb
Matan Nassau <matan.nassau@gmail.com>
2015-04-08 19:27:43 -0400

    test width::value_type

--- test/width.cc
+++ test/width.cc
@@ -10,3 +10,9 @@ TEST_CASE("direct-initialized width equals the value it was initialized to") {
   tls::width w{4};
   REQUIRE(w == 4);
 }
+
+TEST_CASE("width::value_type compiles") {
+  tls::width::value_type x{4};
+  tls::width w{4};
+  REQUIRE(x == w);
+}
