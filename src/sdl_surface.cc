0f569179fb167f68c60d60276318f4c4b49a58ec
Matan Nassau <matan.nassau@gmail.com>
2015-08-30 03:08:00 -0400

    sdl_surface loads from a bmp filename

--- src/sdl_surface.cc
+++ src/sdl_surface.cc
@@ -2,6 +2,9 @@
 #include <SDL2/SDL.h>
 
 namespace tls {
+sdl_surface::sdl_surface(std::string const& bmp_filename)
+: s(SDL_LoadBMP(bmp_filename.c_str()), &SDL_FreeSurface) {}
+
 sdl_surface::sdl_surface(SDL_Surface* s) : s{s, SDL_FreeSurface} {}
 
 SDL_Surface* sdl_surface::get() const { return s.get(); }
