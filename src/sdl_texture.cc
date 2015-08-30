48f430e75d818b109e9386353aca98558f075eb2
Matan Nassau <matan.nassau@gmail.com>
2015-08-30 03:30:40 -0400

    construct texture from surface

--- src/sdl_texture.cc
+++ src/sdl_texture.cc
@@ -1,8 +1,15 @@
 #include "sdl_texture.hh"
+#include "sdl_renderer.hh"
+#include "sdl_surface.hh"
 #include <SDL2/SDL.h>
 
 namespace tls {
 sdl_texture::sdl_texture(SDL_Texture* t) : t{t, &SDL_DestroyTexture} {}
+
+sdl_texture::sdl_texture(sdl_renderer& r, sdl_surface& s)
+: t(SDL_CreateTextureFromSurface(r.get(), s.get()), &SDL_DestroyTexture)
+{
+}
 
 SDL_Texture* sdl_texture::get() const { return t.get(); }
 }
--- src/sdl_texture.hh
+++ src/sdl_texture.hh
@@ -3,10 +3,13 @@
 
 #include <memory>
 #include <SDL2/SDL.h>
+#include "sdl_surface_fwd.hh"
+#include "sdl_renderer_fwd.hh"
 
 namespace tls {
 struct sdl_texture {
   explicit sdl_texture(SDL_Texture* t);
+  sdl_texture(sdl_renderer& r, sdl_surface& s);
 
   SDL_Texture* get() const;
 
