b45335ab454f35391caeb89a6bc8e2ee1160b8f7
Matan Nassau <matan.nassau@gmail.com>
2015-09-01 18:56:48 -0400

    mv crossing() bounding()

--- test/point_bounding.cc
+++ test/point_bounding.cc
@@ -4,62 +4,62 @@
 #include "../src/dim/width.hh"
 #include "../src/dim/height.hh"
 
-TEST_CASE("crossing() outside a zero-sized positioned_rectangle") {
+TEST_CASE("bounding() outside a zero-sized positioned_rectangle") {
   tls::point const p{1, 1};
   tls::positioned_rectangle const r{
     tls::point{0, 0}, tls::rectangle{tls::dim::width{0}, tls::dim::height{0}}};
-  REQUIRE(!tls::crossing(p, r));
+  REQUIRE(!tls::bounding(p, r));
 }
 
-TEST_CASE("crossing() on a zero-sized positioned_rectangle") {
+TEST_CASE("bounding() on a zero-sized positioned_rectangle") {
   tls::point const p{0, 0};
   tls::positioned_rectangle const r{
     tls::point{0, 0}, tls::rectangle{tls::dim::width{0}, tls::dim::height{0}}};
-  REQUIRE(tls::crossing(p, r));
+  REQUIRE(tls::bounding(p, r));
 }
 
-TEST_CASE("crossing() inside a non-zero-sized positioned_rectangle") {
+TEST_CASE("bounding() inside a non-zero-sized positioned_rectangle") {
   tls::point const p{1, 1};
   tls::positioned_rectangle const r{
     tls::point{0, 0}, tls::rectangle{tls::dim::width{2}, tls::dim::height{2}}};
-  REQUIRE(!tls::crossing(p, r));
+  REQUIRE(!tls::bounding(p, r));
 }
 
-TEST_CASE("crossing() outside a non-zero-sized positioned_rectangle") {
+TEST_CASE("bounding() outside a non-zero-sized positioned_rectangle") {
   tls::point const p{0, 0};
   tls::positioned_rectangle const r{
     tls::point{1, 1}, tls::rectangle{tls::dim::width{2}, tls::dim::height{2}}};
-  REQUIRE(!tls::crossing(p, r));
+  REQUIRE(!tls::bounding(p, r));
 }
 
 TEST_CASE(
-  "crossing() on the top bound of a non-zero-sized positioned_rectangle") {
+  "bounding() on the top bound of a non-zero-sized positioned_rectangle") {
   tls::point const p{1, 0};
   tls::positioned_rectangle const r{
     tls::point{0, 0}, tls::rectangle{tls::dim::width{2}, tls::dim::height{2}}};
-  REQUIRE(tls::crossing(p, r));
+  REQUIRE(tls::bounding(p, r));
 }
 
 TEST_CASE(
-  "crossing() on the right bound of a non-zero-sized positioned_rectangle") {
+  "bounding() on the right bound of a non-zero-sized positioned_rectangle") {
   tls::point const p{2, 1};
   tls::positioned_rectangle const r{
     tls::point{0, 0}, tls::rectangle{tls::dim::width{2}, tls::dim::height{2}}};
-  REQUIRE(tls::crossing(p, r));
+  REQUIRE(tls::bounding(p, r));
 }
 
 TEST_CASE(
-  "crossing() on the bottom bound of a non-zero-sized positioned_rectangle") {
+  "bounding() on the bottom bound of a non-zero-sized positioned_rectangle") {
   tls::point const p{1, 2};
   tls::positioned_rectangle const r{
     tls::point{0, 0}, tls::rectangle{tls::dim::width{2}, tls::dim::height{2}}};
-  REQUIRE(tls::crossing(p, r));
+  REQUIRE(tls::bounding(p, r));
 }
 
 TEST_CASE(
-  "crossing() on the left bound of a non-zero-sized positioned_rectangle") {
+  "bounding() on the left bound of a non-zero-sized positioned_rectangle") {
   tls::point const p{0, 1};
   tls::positioned_rectangle const r{
     tls::point{0, 0}, tls::rectangle{tls::dim::width{2}, tls::dim::height{2}}};
-  REQUIRE(tls::crossing(p, r));
+  REQUIRE(tls::bounding(p, r));
 }
