2272bc227433e1156062b3b677c78f1de28b08fe
Matan Nassau <matan.nassau@gmail.com>
2015-09-03 10:52:06 -0400

    detail::window

---
+++ src/detail/window.cc
@@ -0,0 +1,32 @@
+#include "window.hh"
+#include <SDL2/SDL.h>
+#include "../window_creation_error.hh"
+#include "../positioned_rectangle.hh"
+#include "../rectangle.hh"
+#include "../point.hh"
+
+namespace tls {
+namespace detail {
+window::window(char const* const title,
+               positioned_rectangle const& bounds,
+               int const& flags)
+: w{SDL_CreateWindow(title,
+                     bounds.top_left().x,
+                     bounds.top_left().y,
+                     bounds.width(),
+                     bounds.height(),
+                     flags),
+    &SDL_DestroyWindow} {
+  if(w == nullptr) throw window_creation_error(SDL_GetError());
+}
+
+window::window(char const* const title, rectangle dimensions, int const& flags)
+: window(
+    title,
+    positioned_rectangle(
+      point(SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED), dimensions),
+    flags) {}
+
+SDL_Window* window::get() const { return w.get(); }
+}
+}
