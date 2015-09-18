a7f6a3313ff537c31fd6bb9c64ecd32e03e1a28a
Matan Nassau <matan.nassau@gmail.com>
2015-09-18 09:53:32 -0400

    surface ttf ctor

--- src/surface.cc
+++ src/surface.cc
@@ -1,9 +1,20 @@
 #include "surface.hh"
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_ttf.h>
 #include "surface_creation_error.hh"
 #include "bmp_filename.hh"
 #include "width.hh"
 #include "height.hh"
+#include "text.hh"
+#include "colour.hh"
+#include "ttf_font.hh"
+#include "ttf_filename.hh"
+
+namespace {
+SDL_Color sdl_colour(gls::colour const& c) {
+  return {c.red(), c.green(), c.blue(), c.alpha()};
+}
+}
 
 namespace gls {
 surface::surface(SDL_Surface* s) : s(s, &SDL_FreeSurface) {
@@ -13,13 +24,12 @@ surface::surface(SDL_Surface* s) : s(s, &SDL_FreeSurface) {
 surface::surface(bmp_filename const& bmp)
 : surface(SDL_LoadBMP(bmp.c_str())) {}
 
-dim::width surface::width() const {
-  return dim::width(s->w);
-}
+surface::surface(ttf_font const& ttf, text const& t, colour const& c)
+: surface(TTF_RenderText_Solid(ttf.get(), t.c_str(), sdl_colour(c))) {}
 
-dim::height surface::height() const {
-  return dim::height(s->h);
-}
+dim::width surface::width() const { return dim::width(s->w); }
+
+dim::height surface::height() const { return dim::height(s->h); }
 
 SDL_Surface* surface::get() const { return s.get(); }
 }
