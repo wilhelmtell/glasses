d84a922782809f8afb3d1bd72e167b155333de66
Matan Nassau <matan.nassau@gmail.com>
2015-09-18 09:22:32 -0400

    surface::width(), height()

--- src/surface.hh
+++ src/surface.hh
@@ -4,12 +4,17 @@
 #include <memory>
 #include <SDL2/SDL.h>
 #include "bmp_filename_fwd.hh"
+#include "width_fwd.hh"
+#include "height_fwd.hh"
 
 namespace gls {
 struct surface {
   surface() = default;
   explicit surface(SDL_Surface* s);
   explicit surface(bmp_filename const& bmp);
+
+  dim::width width() const;
+  dim::height height() const;
 
   SDL_Surface* get() const;
 
