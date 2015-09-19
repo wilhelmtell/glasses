feb6833129de2b311371ddf20fcd662ba0a002ff
Matan Nassau <matan.nassau@gmail.com>
2015-09-19 12:54:09 -0400

    text constructed with a c string equals c_str()

--- test/text.cc
+++ test/text.cc
@@ -1,5 +1,6 @@
 #include <catch/catch.hpp>
 #include "../src/text.hh"
+#include <string>
 
 TEST_CASE("default-constructed text compiles") {
   gls::text t;
@@ -7,4 +8,9 @@ TEST_CASE("default-constructed text compiles") {
 
 TEST_CASE("text constructed with a C string compiles") {
   gls::text const t("a");
+}
+
+TEST_CASE("1 character text constructed with a C string equals c_str()") {
+  gls::text const t("a");
+  REQUIRE(std::string("a") == t.c_str());
 }
