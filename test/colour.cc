b774fb51816e2e740e739a5f7afb0c838473ee25
Matan Nassau <matan.nassau@gmail.com>
2015-09-19 15:05:51 -0400

    value-init color zeroes out

--- test/colour.cc
+++ test/colour.cc
@@ -4,3 +4,9 @@
 TEST_CASE("default-constructed colour compiles") {
   gls::colour t;
 }
+
+TEST_CASE("value-initialized colour zeroes out") {
+  gls::colour const t{};
+  auto const them_all = t.red() | t.green() | t.blue() | t.alpha();
+  REQUIRE(them_all == 0);
+}
