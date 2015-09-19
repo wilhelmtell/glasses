41b0e111cb77652607e8e23874b7a00ea160ae49
Matan Nassau <matan.nassau@gmail.com>
2015-09-19 14:21:13 -0400

    empty text equals its c_str()

--- test/text.cc
+++ test/text.cc
@@ -14,3 +14,8 @@ TEST_CASE("1 character text constructed with a C string equals c_str()") {
   gls::text const t("a");
   REQUIRE(std::string("a") == t.c_str());
 }
+
+TEST_CASE("empty text constructed with a C string equals c_str()") {
+  gls::text const t("");
+  REQUIRE(std::string("") == t.c_str());
+}
