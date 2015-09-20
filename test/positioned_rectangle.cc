ea303db1feda49bf7cf1fe80a04f305c8efb9306
Matan Nassau <matan.nassau@gmail.com>
2015-09-20 10:59:44 -0400

    test equality of 0-size rects

--- test/positioned_rectangle.cc
+++ test/positioned_rectangle.cc
@@ -62,3 +62,22 @@ TEST_CASE("positioned_rectangle equality with 2 2nd-quadrant rects") {
     gls::point(1, 1), gls::rectangle(gls::dim::width(2), gls::dim::height(3)));
   REQUIRE(a == b);
 }
+
+TEST_CASE("positioned_rectangle equality with 2 0-sized rects") {
+  gls::positioned_rectangle const a(
+    gls::point(0, 0), gls::rectangle(gls::dim::width(0), gls::dim::height(0)));
+  gls::positioned_rectangle const b(
+    gls::point(0, 0), gls::rectangle(gls::dim::width(0), gls::dim::height(0)));
+  REQUIRE(a == b);
+}
+
+TEST_CASE(
+  "positioned_rectangle inequality with 2 0-sized, different positions, "
+  "rects") {
+  gls::positioned_rectangle const a(
+    gls::point(0, 0), gls::rectangle(gls::dim::width(0), gls::dim::height(0)));
+  gls::positioned_rectangle const b(
+    gls::point(1, -1),
+    gls::rectangle(gls::dim::width(0), gls::dim::height(0)));
+  REQUIRE(a != b);
+}
