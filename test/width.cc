fc7399fdc93f777db69abdf4e9234228fbb90899
Matan Nassau <matan.nassau@gmail.com>
2015-09-21 13:47:26 -0400

    test width operator-

--- test/width.cc
+++ test/width.cc
@@ -124,3 +124,15 @@ TEST_CASE("operator+(width) with a negative rhs") {
 TEST_CASE("operator+(width) with a 0 rhs") {
   REQUIRE(gls::dim::width(4) + 0 == 4);
 }
+
+TEST_CASE("operator-(width) with a positive rhs") {
+  REQUIRE(gls::dim::width(4) - 3 == 1);
+}
+
+TEST_CASE("operator-(width) with a negative rhs") {
+  REQUIRE(gls::dim::width(4) - -3 == 7);
+}
+
+TEST_CASE("operator-(width) with a 0 rhs") {
+  REQUIRE(gls::dim::width(4) - 0 == 4);
+}
