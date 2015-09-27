271259a3bf9bbce31255d035318e5f176c91581f
Matan Nassau <matan.nassau@gmail.com>
2015-09-27 09:42:36 -0400

    renderer ctor delegates, requires not null

--- src/renderer.cc
+++ src/renderer.cc
@@ -3,11 +3,12 @@
 #include <SDL2/SDL.h>
 
 namespace gls {
-renderer::renderer(SDL_Renderer* r) : r(r, &SDL_DestroyRenderer) {}
+renderer::renderer(SDL_Renderer* r) : r(r, &SDL_DestroyRenderer) {
+  if(r == nullptr) throw renderer_creation_error(SDL_GetError());
+}
 
 renderer::renderer(SDL_Window* w)
-: r(SDL_CreateRenderer(w, -1, 0), &SDL_DestroyRenderer) {
-  if(r == nullptr) throw renderer_creation_error(SDL_GetError());
+: renderer(SDL_CreateRenderer(w, -1, 0)) {
 }
 
 SDL_Renderer* renderer::get() const { return r.get(); }
