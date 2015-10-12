2718f3e9583393ee7438e5a1968b00f054ef11a8
Matan Nassau <matan.nassau@gmail.com>
2015-10-12 16:42:56 -0400

    mix_chunk flac_filename ctor

--- src/mix_chunk.cc
+++ src/mix_chunk.cc
@@ -2,6 +2,7 @@
 #include "mix_chunk_open_error.hh"
 #include <SDL2/SDL_mixer.h>
 #include "wav_filename.hh"
+#include "flac_filename.hh"
 
 namespace gls {
 mix_chunk::mix_chunk(Mix_Chunk* chunk) : c(chunk, &Mix_FreeChunk) {
@@ -9,6 +10,9 @@ mix_chunk::mix_chunk(Mix_Chunk* chunk) : c(chunk, &Mix_FreeChunk) {
 }
 
 mix_chunk::mix_chunk(wav_filename const& name)
+: mix_chunk(Mix_LoadWAV(name.c_str())) {}
+
+mix_chunk::mix_chunk(flac_filename const& name)
 : mix_chunk(Mix_LoadWAV(name.c_str())) {}
 
 Mix_Chunk* mix_chunk::get() const { return c.get(); }
