bae1ddf5c1ac2235df1a86edfa149084ca827469
Matan Nassau <matan.nassau@gmail.com>
2015-10-12 15:11:28 -0400

    png_filename is a basic_filename<>

--- src/png_filename_fwd.hh
+++ src/png_filename_fwd.hh
@@ -1,8 +1,11 @@
 #ifndef GLS_PNG_FILENAME_FWD_HH_
 #define GLS_PNG_FILENAME_FWD_HH_
 
+#include "detail/basic_filename_fwd.hh"
+
 namespace gls {
-struct png_filename;
+struct png_filename_tag;
+using png_filename = detail::basic_filename<png_filename_tag>;
 }
 
 #endif
