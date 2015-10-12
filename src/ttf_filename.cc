6ebc3c6002c99a05835e0cf33598dff9da56245a
Matan Nassau <matan.nassau@gmail.com>
2015-10-12 15:14:35 -0400

    ttf_filename is a basic_filename<>

--- src/ttf_filename.cc
+++
@@ -1,8 +0,0 @@
-#include "ttf_filename.hh"
-#include <string>
-
-namespace gls {
-ttf_filename::ttf_filename(std::string const& name) : name(name) {}
-
-char const* ttf_filename::c_str() const { return name.c_str(); }
-}
