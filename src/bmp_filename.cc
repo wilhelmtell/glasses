4ed02c4ff7b226fe02203d791df91284f723aefd
Matan Nassau <matan.nassau@gmail.com>
2015-10-12 15:07:55 -0400

    bmp_filename is a basic_filename<>

--- src/bmp_filename.cc
+++
@@ -1,8 +0,0 @@
-#include "bmp_filename.hh"
-#include <string>
-
-namespace gls {
-bmp_filename::bmp_filename(std::string const& name) : name(name) {}
-
-char const* bmp_filename::c_str() const { return name.c_str(); }
-}
