8cbf0200e11c34a493e7792d05317995b0674f2a
Matan Nassau <matan.nassau@gmail.com>
2015-10-16 16:26:24 -0400

    rm redundanry declarations

--- src/sdl_point.hh
+++ src/sdl_point.hh
@@ -7,9 +7,6 @@
 namespace gls {
 bool equal(SDL_Point const& lhs, SDL_Point const& rhs);
 bool not_equal(SDL_Point const& lhs, SDL_Point const& rhs);
-bool bounding(SDL_Point const& p, SDL_Rect const& b);
-bool outside(SDL_Point const& p, SDL_Rect const& b);
-bool inside(SDL_Point const& p, SDL_Rect const& b);
 SDL_Point origin_point();
 SDL_Point shifted_up(SDL_Point const& p, int const& offset);
 SDL_Point shifted_down(SDL_Point const& p, int const& offset);
