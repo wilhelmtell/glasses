c48905e632b6a3413a447571b5f8bf6dbd72711b
Matan Nassau <matan.nassau@gmail.com>
2015-10-16 15:39:42 -0400

    test shifted_up(origin_point(), 1)

--- test/sdl_point.cc
+++ test/sdl_point.cc
@@ -6,3 +6,9 @@ TEST_CASE("origin_point() returns the (0,0) point") {
   REQUIRE(p.x == 0);
   REQUIRE(p.y == 0);
 }
+
+TEST_CASE("shifted_up(origin_point(), 1) is (0,-1)") {
+  auto const p = gls::shifted_up(gls::origin_point(), 1);
+  REQUIRE(p.x == 0);
+  REQUIRE(p.y == -1);
+}
