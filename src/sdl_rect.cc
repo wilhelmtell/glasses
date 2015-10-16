02bcdce78306c6dc7368bcee92313025ea5a893a
Matan Nassau <matan.nassau@gmail.com>
2015-10-16 19:03:55 -0400

    rephrase intersect(rect,rect)

--- src/sdl_rect.cc
+++ src/sdl_rect.cc
@@ -32,10 +32,10 @@ SDL_Rect q4_rectangle() {
 }
 
 bool intersect(SDL_Rect const& a, SDL_Rect const& b) {
-  return inside(top_left(a), b) || inside(bottom_right(a), b)
-         || bounding(top_left(a), b) || bounding(bottom_right(a), b)
-         || inside(top_right(a), b) || inside(bottom_left(a), b)
-         || bounding(top_right(a), b) || bounding(bottom_left(a), b);
+  return inside(top_left(a), b) || bounding(top_left(a), b)
+         || inside(bottom_right(a), b) || bounding(bottom_right(a), b)
+         || inside(top_right(a), b) || bounding(top_right(a), b)
+         || inside(bottom_left(a), b) || bounding(bottom_left(a), b);
 }
 
 bool outside(SDL_Rect const& a, SDL_Rect const& b) {
