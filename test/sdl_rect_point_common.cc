867caeea5a05767f658cb24b3eea6f513c23d7bc
Matan Nassau <matan.nassau@gmail.com>
2015-10-16 16:46:44 -0400

    test at()

--- test/sdl_rect_point_common.cc
+++ test/sdl_rect_point_common.cc
@@ -1,2 +1,24 @@
 #include <catch/catch.hpp>
 #include "../src/sdl_rect_point_common.hh"
+#include "../src/sdl_point.hh"
+#include "../src/sdl_rect.hh"
+
+TEST_CASE("at()") {
+  auto const p = gls::origin_point();
+  SECTION("true at()") {
+    auto const r = gls::unit_rectangle();
+    REQUIRE(gls::at(r, p));
+  }
+  SECTION("false at()") {
+    SDL_Rect const r{1, 1, 1, 1};
+    REQUIRE(!gls::at(r, p));
+  }
+  SECTION("point inside rect is not at() rect") {
+    SDL_Rect const r{-1, -1, 2, 2};
+    REQUIRE(!gls::at(r, p));
+  }
+  SECTION("point merely bounding with rect is not at() rect") {
+    SDL_Rect const r{0, -1, 2, 2};
+    REQUIRE(!gls::at(r, p));
+  }
+}
